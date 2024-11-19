0586a5ea8929883c88e318d1ea0554e95b638323
ThanhDatThanhDat <dt2229992@gmail.com>
2024-11-19 22:06:40 +0700

    ss2-2

---
+++ ss2-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	// Khai bao bien
+	int firstNumber = 123;
+	int seconNumber = 234;
+	int total = firstNumber + seconNumber;
+	
+	// Tinh ket qua va in ra man hinh
+	printf("total = %d", total,firstNumber,seconNumber);
+	
+	return 0;
+}
